fc58e4a1220b9136026382ff36bf03384c5e091f
Alex Mitzman and Eileen McFarland <pair+alex+eileen@allstate.com>
2016-06-24 13:39:55 -0500

    fix post in persons controller

--- src/controllers/persons.js
+++ src/controllers/persons.js
@@ -1,6 +1,7 @@
 /* eslint-disable new-cap */
 
 import express from 'express';
+import Person from '../models/person';
 const router = module.exports = express.Router();
 
 router.get('/', (req, res) => {
@@ -11,8 +12,10 @@ router.get('/new', (req, res) => {
   res.render('person/new');
 });
 
-router.post('/new', (req, res) => {
-  res.redirect('person/index');
+router.post('/', (req, res) => {
+  const person = new Person(req.body);
+  person.save();
+  res.redirect('/person');
 });
 
 router.get('/:id/show', (req, res) => {
