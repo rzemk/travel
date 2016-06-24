a4864099f70cedffebe48d768913fed2df36271f
Bal Saini and Rich Zemke <pair+bal+rich@allstate.com>
2016-06-24 14:38:18 -0500

    wip

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -14,3 +14,18 @@ router.get('/about', (req, res) => {
 router.get('/faq', (req, res) => {
   res.render('home/faq');
 });
+
+// router.get('/person', (req, res) => {
+//   res.render('person/new');
+// });
+//
+// router.post('/person', (req, res) => {
+//   console.log('req', req.body);
+//   res.render('person/index');
+// });
+//
+// router.get('/person/index', (req, res) => {
+//   const people = [];
+//   people.push({ name: 'bob', })
+//   res.render('person/new', people);
+// });
