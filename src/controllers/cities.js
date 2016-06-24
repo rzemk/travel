b44aa29b11098e591fbc4a4d955de05610deacdc
Bal Saini and Rich Zemke <pair+bal+rich@allstate.com>
2016-06-24 15:16:45 -0500

    cities

--- src/controllers/cities.js
+++ src/controllers/cities.js
@@ -1,24 +1,28 @@
-/* eslint-disable new-cap */
+/* eslint-disable new-cap, array-callback-return*/
 
 import express from 'express';
 import City from '../models/city';
 const router = module.exports = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('countries/index');
+  City.find((err, cities) => {
+    res.render('cities/index', { cities });
+  });
 });
 
 router.get('/new', (req, res) => {
-  res.render('countries/new');
+  res.render('cities/new');
 });
 
 router.post('/', (req, res) => {
-  const country = new Country(req.body);
-  country.save(() => {
-    res.redirect('/countries');
+  const city = new City(req.body);
+  city.save(() => {
+    res.redirect('/cities');
   });
 });
 
 router.get('/:id/show', (req, res) => {
-  res.render('countries/show');
+  City.findById(req.params.id, (err, city) => {
+    res.render('cities/show', { city });
+  });
 });
